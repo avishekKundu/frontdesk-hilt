f3b262d315a0fd911b52f720dc3638378efbb2fd
avishekIdeagensys <avishek.kundu@ideagensys.com>
2025-10-23 12:13:03 +0530

    Added files

---
+++ src/main/java/com/example/frontdesk_hilt/FrontdeskHiltApplication.java
@@ -0,0 +1,13 @@
+package com.example.frontdesk_hilt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontdeskHiltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontdeskHiltApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/frontdesk_hilt/FrontdeskHiltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frontdesk_hilt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontdeskHiltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
